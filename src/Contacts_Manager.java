86978a4330c79cb793965f0c3b2d7cf33d124811
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-06-29 18:09:43 -0400

    project complete

--- src/Contacts_Manager.java
+++ src/Contacts_Manager.java
@@ -7,16 +7,21 @@ import java.util.*;
 
 
 public class Contacts_Manager {
+//    initializing the path to contacts file
     public static String filename = "contacts.txt";
+//    empty list for delete method to put all the contacts into EXCEPT the chosen contact
     public static List<String> deleteList = new ArrayList<>();
-
+// This method will render all contacts in contacts file path
     public static void renderAll() {
+//        header for display chart when rendered
         System.out.println("Name    | Phone Number\n----------------------");
         try {
+//            IF the file has not yet been created then it will create it
             Path datafile = Paths.get(filename);
             if (Files.notExists(datafile)) {
                 Files.createFile(datafile);
             }
+//            List of contacts to display using a for loop
             List<String> files = Files.readAllLines(datafile);
             for (String file : files) {
                 System.out.println(file);
@@ -25,7 +30,7 @@ public class Contacts_Manager {
             iox.printStackTrace();
         }
     }
-
+// add contact method
     public static void addContact() {
         try {
             Path dataFile = Paths.get(filename);
@@ -36,6 +41,7 @@ public class Contacts_Manager {
             System.out.println("Add a contact number");
             String number = sc.next();
             Contacts newContact = new Contacts(name, number);
+//            This specific format to set the table properly
             String contact = String.format("%-7s | %s",newContact.getName(),newContact.getPhoneNumber());
 
             Files.write(dataFile, Arrays.asList(contact), StandardOpenOption.APPEND);
@@ -45,7 +51,7 @@ public class Contacts_Manager {
         }
 
     }
-
+// search method
     public static void search() {
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("\n");
@@ -82,6 +88,7 @@ public class Contacts_Manager {
                     System.out.println("Deleting: " + name);
                     continue;
                 }
+//                adds all other contacts EXCEPT chosen into new list to render
                 deleteList.add(name);
             }
             Files.write(data, deleteList);
@@ -90,7 +97,7 @@ public class Contacts_Manager {
         }
 
     }
-
+// The main menu * where everything gets put together *
     public static void mainMenu() {
         Scanner sc = new Scanner(System.in);
             System.out.println("1. View contacts\n2. Add a new contact\n3. Search a contact by name\n4. Delete an existing contact\n5. Exit\nEnter a number!");
@@ -116,12 +123,14 @@ public class Contacts_Manager {
     public static void main(String[] args) {
         Path dataFile = Paths.get(filename);
         List<Contacts> contacts = new ArrayList<>();
+//        Initial contacts to start off with
         contacts.add(new Contacts("Miguel", "9789831339"));
         contacts.add(new Contacts("Isiah", "9788354917"));
         contacts.add(new Contacts("Rin", "5106557707"));
         contacts.add(new Contacts("Senju", "8005882300"));
         List<String> contactStrings = new ArrayList<>();
         for (Contacts contact : contacts) {
+//            Format to display table properly
             contactStrings.add(String.format("%-7s | %s",contact.getName(),contact.getPhoneNumber()));
         }
         try {
